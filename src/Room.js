713c3f5437aa876c99f90d73c67bc2a24cd09278
Bharath <bharathraj7302@gmail.com>
2023-04-12 19:56:26 +0530

    initial commit

---
+++ src/Room.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+
+const Room = () => {
+  const { roomID } = useParams();
+  const meeting = async (element) => {
+    const appID = 1856965929;
+    const serverSecret = "0fbc8e68887dd4467ec0206e453236aa";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      Date.now().toString(),
+      "Bharath"
+    );
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+    zp.joinRoom({
+      container: element,
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall,
+      },
+    });
+  };
+
+  return <div ref={meeting} style={{ width: "100vw", height: "100vh" }}></div>;
+};
+
+export default Room;
